93d4fa11198317162620b9edd16d7923cacfa210
kmomiji <yakisoba797@i.softbank.jp>
2026-05-01 14:20:37 +0900

    aframeARにraycasterを追加してみるテスト

--- docs/assets/components/three/threefirst.js
+++ docs/assets/components/three/threefirst.js
@@ -3,24 +3,25 @@ import * as THREE from 'three'
 function main() {
     // キャンバス・レンダラーの指定
     const canvas = document.querySelector('#cv')
-    const renderer = new THREE.WebGLRenderer({antialias: true, canvas})
+    let renderer = new THREE.WebGLRenderer({antialias: true, canvas})
+    renderer.xr.enabled = true
 
     const fov = 80
     const aspect = 2
     const near = 0.1
     const far = 100
+    // カメラ設定
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
     camera.position.z = 2
 
     const scene = new THREE.Scene()
-
-    {
-        const color = 0xFFFFFF
-        const intensity = 3
-        const light = new THREE.DirectionalLight(color, intensity)
-        light.position.set(-1, 2, 4)
-        scene.add(light)
-    }
+    
+    const color = 0xFFFFFF
+    const intensity = 3
+    const light = new THREE.DirectionalLight(color, intensity)
+    light.position.set(-1, 2, 4)
+    scene.add(light)
+    
 
     const boxWidth = 1
     const boxHeight = 1
